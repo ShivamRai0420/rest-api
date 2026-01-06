a7bb9a3924eebc3090a46d096f2b08f55a4494f4
ShivamRai <shivamraid232002@gmail.com>
2026-01-07 00:08:25 +0530

    jar to war

--- src/main/java/com/example/rest_api/RestApiApplication.java
+++ src/main/java/com/example/rest_api/RestApiApplication.java
@@ -2,13 +2,17 @@ package com.example.rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApiApplication {
-
+public class RestApiApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestApiApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
         System.out.println("Application Started Rest-Api");
 	}
-
 }
